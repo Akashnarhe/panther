6ea4b977be480e17c9f48569336d2deea51122a6
Akash Narhe <akash.narhe@capgemini.com>
2019-07-04 14:13:43 +0530

    tp

--- SpringBoot-multi-module-dao/src/main/java/com/example/daos/EmployeeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.daos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.entities.Employee;
-
-public interface EmployeeeRepository extends JpaRepository<Employee, Integer>{
-
-}
